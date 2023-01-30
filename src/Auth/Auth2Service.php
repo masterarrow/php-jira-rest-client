9327aec71162d778c29650e93e39d37b890864bb
Arrow <36331175+masterarrow@users.noreply.github.com>
2023-01-30 10:51:21 +0200

    Update Auth2Service.php

--- src/Auth/Auth2Service.php
+++ src/Auth/Auth2Service.php
@@ -145,7 +145,7 @@ class Auth2Service extends Jira
     {
         $response = Http::accept('application/json')
             ->withToken($this->token->getToken())
-            ->get($this->getRefreshTokenUrl());
+            ->get($this->getRefreshTokenUrl() . '/accessible-resources');
 
         if ($response->status() === 200) {
             return json_decode($response->body(), true);
